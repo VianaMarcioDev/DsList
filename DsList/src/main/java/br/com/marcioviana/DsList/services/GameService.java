ad51c221217e06b8ac102909772df8ade8be8547
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-16 20:38:02 -0300

    Criar metodo findById

--- DsList/src/main/java/br/com/marcioviana/DsList/services/GameService.java
+++ DsList/src/main/java/br/com/marcioviana/DsList/services/GameService.java
@@ -1,10 +1,12 @@
 package br.com.marcioviana.DsList.services;
 
+import br.com.marcioviana.DsList.dto.GameDTO;
 import br.com.marcioviana.DsList.dto.GameMinDTO;
 import br.com.marcioviana.DsList.entities.Game;
 import br.com.marcioviana.DsList.repositories.Gamerepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,9 +15,16 @@ public class GameService {
     @Autowired
     Gamerepository gamerepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gamerepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gamerepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
         return dto;
     }
 }
