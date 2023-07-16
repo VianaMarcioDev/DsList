e1b253061737e8b9459be0b3df8afcc0b4f3db5e
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-15 22:43:25 -0300

    Primeiro commit. Criar projeto.

---
+++ DsList/src/main/java/br/com/marcioviana/DsList/DsListApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.DsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListApplication.class, args);
+	}
+
+}
---
+++ DsList/src/test/java/br/com/marcioviana/DsList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.DsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
