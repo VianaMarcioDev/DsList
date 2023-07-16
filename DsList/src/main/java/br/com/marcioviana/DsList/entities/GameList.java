204c17b22936ad68dd752f4a44498a44a2ffdc43
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-16 20:15:32 -0300

    Mapear classe GameList

--- DsList/src/main/java/br/com/marcioviana/DsList/entities/GameList.java
+++ DsList/src/main/java/br/com/marcioviana/DsList/entities/GameList.java
@@ -1,8 +1,14 @@
 package br.com.marcioviana.DsList.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name ="tb_game_list")
 public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
