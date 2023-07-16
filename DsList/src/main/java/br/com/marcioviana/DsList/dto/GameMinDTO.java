a3849f713fe01e6cbe0eed8fb1e4bfabdc6e25a0
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-15 23:35:29 -0300

    Criar classe de response GameMinDTO

---
+++ DsList/src/main/java/br/com/marcioviana/DsList/dto/GameMinDTO.java
@@ -0,0 +1,46 @@
+package br.com.marcioviana.DsList.dto;
+
+
+import br.com.marcioviana.DsList.entities.Game;
+
+public class GameMinDTO {
+    private Long id;
+    private String title;
+
+    private Integer year;
+
+    private String imgUrl;
+
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Game entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.year = entity.getYear();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
