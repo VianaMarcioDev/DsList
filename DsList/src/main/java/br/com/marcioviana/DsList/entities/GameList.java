1f36b339647d4a3a1e34b1c40986d436e25e7c72
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-16 19:48:26 -0300

    Criar classe GameList

---
+++ DsList/src/main/java/br/com/marcioviana/DsList/entities/GameList.java
@@ -0,0 +1,45 @@
+package br.com.marcioviana.DsList.entities;
+
+import java.util.Objects;
+
+public class GameList {
+    private Long id;
+    private String name;
+
+    public GameList() {
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
