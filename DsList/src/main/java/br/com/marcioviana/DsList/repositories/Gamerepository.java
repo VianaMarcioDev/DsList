71f3ce4e94d135b2cbdafb7fd8244774b349b59e
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-16 21:56:36 -0300

    Criar consulta SQL em Gamerepository

--- DsList/src/main/java/br/com/marcioviana/DsList/repositories/Gamerepository.java
+++ DsList/src/main/java/br/com/marcioviana/DsList/repositories/Gamerepository.java
@@ -2,6 +2,16 @@ package br.com.marcioviana.DsList.repositories;
 
 import br.com.marcioviana.DsList.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface Gamerepository  extends JpaRepository<Game, Long> {
+    @Query(nativeQuery = true, value = """
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		tb_game.short_description AS shortDescription, tb_belonging.position
+		FROM tb_game
+		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+		WHERE tb_belonging.list_id = :listId
+		ORDER BY tb_belonging.position
+			""")
+    List<GameMinProjection> searchByList(Long listId);
 }
