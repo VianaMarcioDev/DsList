ca467c3100e87b01624942840942bc5b0bce0d73
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-16 23:11:48 -0300

    Alterar parametro da consulta SQL para funcionar com Postgres

--- DsList/src/main/java/br/com/marcioviana/DsList/dto/GameMinDTO.java
+++ DsList/src/main/java/br/com/marcioviana/DsList/dto/GameMinDTO.java
@@ -27,7 +27,7 @@ public class GameMinDTO {
     public GameMinDTO(GameMinProjection projection) {
         this.id = projection.getId();
         this.title = projection.getTitle();
-        this.year = projection.getYear();
+        this.year = projection.getGameYear();
         this.imgUrl = projection.getImgUrl();
         this.shortDescription = projection.getShortDescription();
     }
