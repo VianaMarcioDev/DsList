9a5545a9b2fde5ab90ab7ce994c0023e2c9e74c2
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-07-16 21:48:36 -0300

    Criar classe GameListController

---
+++ DsList/src/main/java/br/com/marcioviana/DsList/controllers/GameListController.java
@@ -0,0 +1,30 @@
+package br.com.marcioviana.DsList.controllers;
+
+import br.com.marcioviana.DsList.dto.GameDTO;
+import br.com.marcioviana.DsList.dto.GameListDTO;
+import br.com.marcioviana.DsList.dto.GameMinDTO;
+import br.com.marcioviana.DsList.services.GameListService;
+import br.com.marcioviana.DsList.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+
+
+}
